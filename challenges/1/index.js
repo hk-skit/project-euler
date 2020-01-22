bc331e5c9b2d9da7d1967263c9e822f1b4b21d01
Hitesh Kumar <hitesh.kumar2@myntra.com>
2020-01-22 16:30:09 +0530

    Day 1 Solution.

--- challenges/1/index.js
+++ challenges/1/index.js
@@ -1,5 +1,44 @@
-function sum(a, b) {
-  return a + b;
-}
+/**
+ * If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
+ * The sum of these multiples is 23. Find the sum of all the multiples of 3 or 5 below the provided
+ * parameter value number.
+ */
 
-module.exports = sum;
+/**
+ * Brute force solution.
+ * @param {number} n
+ */
+const _sumOfMultipleOf3Or5 = n => {
+  let sum = 0;
+  while (--n > 0) {
+    if (n % 3 === 0 || n % 5 === 0) {
+      sum += n;
+    }
+  }
+  return sum;
+};
+
+const sumOfMultipleOfX = (n, x) => {
+  // We need to be clever while calculating the sumOfMultipleOfX otherwise our solution
+  // will be slower than brute force solution.
+  // We can use arithmetic progression to calculate the sum.
+  // 3 + 6 + 9 + .... + N
+  // 3 * (1 + 2 + 3 .... + N/3)
+  const terms = Math.floor((n - 1) / x);
+  // Some of n terms of an AP -> n * (n + 1) / 2.
+  return (x * (terms * (terms + 1))) / 2;
+};
+
+/**
+ *
+ * @param {*} n
+ */
+const sumOfMultipleOf3Or5 = n => {
+  // Instead of checking divisibility of every number what if we do
+  // sumOfMultipleOf3 + sumOfMultipleOf5 - sumOfMultipleOf15
+  return (
+    sumOfMultipleOfX(n, 3) + sumOfMultipleOfX(n, 5) - sumOfMultipleOfX(n, 15)
+  );
+};
+
+module.exports = sumOfMultipleOf3Or5;
--- challenges/1/index.test.js
+++ challenges/1/index.test.js
@@ -1,5 +1,30 @@
-const sum = require('./index');
+const sumOfMultipleOf3Or5 = require('./index');
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
+describe('Day 1: sumOfMultipleOf3Or5', () => {
+  [
+    {
+      n: 0,
+      sum: 0
+    },
+    {
+      n: 1000,
+      sum: 233168
+    },
+    {
+      n: 49,
+      sum: 543
+    },
+    {
+      n: 19564,
+      sum: 89301183
+    },
+    {
+      n: 8456,
+      sum: 16687353
+    }
+  ].forEach(({ n, sum }) => {
+    test(`n = ${n}, sum = ${sum}`, () => {
+      expect(sumOfMultipleOf3Or5(n)).toBe(sum);
+    });
+  });
 });
